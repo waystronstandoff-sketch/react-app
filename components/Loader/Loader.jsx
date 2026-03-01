4a2ef7969338fa876a974a70622015f905c6bd45
Floxyyy <waystronstandoff@gmail.com>
2026-03-01 14:05:14 +0400

    loader component

---
+++ components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import cls from './Loader.module.css';
+
+export const Loader = () => {
+
+    return (
+      <div className={cls.backdrop}>
+        <span className={cls.loader}></span>
+      </div>
+    );
+};
