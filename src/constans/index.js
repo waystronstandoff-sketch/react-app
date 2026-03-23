9379a1e20655b97388013643ebf334a45823d5d5
Floxyyy <waystronstandoff@gmail.com>
2026-03-23 19:48:18 +0400

    added .env

--- src/constans/index.js
+++ src/constans/index.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:8801';
+export const API_URL = import.meta.env.VITE_SERVER_URL;
 export const AUTH_STORAGE = 'authStorageKey';
 export const THEME_STORAGE = 'themeStorageKey';
